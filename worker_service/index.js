8e2d2c79bdc62cf86828c8af05ffab04cbc4e114
Jayadeep KM <kmjayadeep@gmail.com>
2019-12-03 07:59:45 +0530

    worker service init
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ worker_service/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const v1 = require("cloudevents-sdk/v1");
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json({ type: 'application/*+json' }))
+const PORT = process.env.PORT || 3000;
+const receiver = new v1.StructuredHTTPReceiver();
+
+app.get('/', (_, res) => {
+  res.send('watermark worker service');
+});
+
+// heartbeat
+app.get('/ping', (_, res) => {
+  res.send('pong');
+})
+
+if (process.env.NODE_ENV == 'development') {
+  app.get('/test-worker', async (_, res) => {
+    res.json('ok');
+  });
+}
+
+app.post('/', (req, res) => {
+  try {
+    const event = receiver.parse(req.body, req.headers);
+    console.log(event);
+    res.status(201).send('Event Accepted');
+  } catch (err) {
+    // TODO deal with errors
+    console.error(err);
+    res.status(415)
+      .header('Content-Type', 'application/json')
+      .send(JSON.stringify(err));
+  }
+});
+
+app.listen(PORT, () => console.log(`worker service app listening on port ${PORT}!`));
