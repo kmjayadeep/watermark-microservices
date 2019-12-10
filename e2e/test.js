a6bbcdf79d74c0223c12e8dc37f28ecf807b30b6
Jayadeep KM <kmjayadeep@gmail.com>
2019-12-10 20:17:36 +0530

    added test cases for ticketing and status services
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- e2e/test.js
+++ e2e/test.js
@@ -1,18 +1,31 @@
-const chakram = require('chakram');
+const chai = require('chai')
 const fetch = require('node-fetch');
 const mlog = require('mocha-logger');
 
-const { expect } = chakram;
+const { expect } = chai;
 
-const ticketingService = 'http://ticketing-service.default.34.82.141.113.nip.io/'
-const statusService = 'http://status-service.default.34.82.141.113.nip.io/';
-const resultService = 'http://status-service.default.34.82.141.113.nip.io/';
+const ticketingService = 'http://ticketing-service.default.34.82.141.113.nip.io'
+const statusService = 'http://status-service.default.34.82.141.113.nip.io';
+const resultService = 'http://status-service.default.34.82.141.113.nip.io';
+const ticketingGraphqlEndpoint = `${ticketingService}/graphql`;
+const statusGraphqlEndpoint = `${statusService}/graphql`;
+
+const graphqlFetch = (endPoint, data) => fetch(endPoint, {
+  headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data),
+  method: "POST",
+}).then(res => res.json());
+
+const sleep = time => new Promise((resolve) => setTimeout(resolve, time));
 
 describe("Ticketing Service", function () {
-  it("Should be running", function () {
-    const response = chakram.get(ticketingService);
-    expect(response).to.have.status(200);
-    return chakram.wait();
+
+  it("Should be running", async function () {
+    const response = await fetch(ticketingService);
+    expect(response.status).to.equal(200);
   });
 
   it('Should return tickets when requesting watermarks using fetch', async function () {
@@ -24,40 +37,83 @@ describe("Ticketing Service", function () {
         }
       }
     `;
-    const response = await fetch("http://ticketing-service.default.34.82.141.113.nip.io/graphql", {
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
+    const response = await graphqlFetch(ticketingGraphqlEndpoint, {
+      query,
+      variables: null
+    })
+    expect(response).to.have.deep.property('data.requestWatermark.ticketId');
+    expect(response).to.have.deep.property('data.requestWatermark.timestamp');
+    expect(response.data.requestWatermark.ticketId).to.be.a('string');
+    expect(response.data.requestWatermark.ticketId).to.not.empty;
+    mlog.success('Got ticketId', response.data.requestWatermark.ticketId, 'for request with', query);
+  });
+
+});
+
+describe("Status Service", function () {
+  it("Should be running", async function () {
+    const response = await fetch(statusService);
+    expect(response.status).to.equal(200);
+  });
+
+  describe('Test if it returns status of requests', function () {
+
+    let ticketId = '';
+
+    before(async () => {
+      mlog.pending('Creating a new request for testing');
+      const query = `
+        mutation {
+          requestWatermark(content:book,title:"my-test-title",author:"me",topic:business) {
+            ticketId
+            timestamp
+          }
+        }
+      `;
+      const response = await graphqlFetch(ticketingGraphqlEndpoint, {
         query,
         variables: null
-      }),
-      "method": "POST",
-    });
-    const result = await response.json();
-    expect(result).to.have.property('data');
-    expect(result.data).to.have.property('requestWatermark');
-    expect(result.data.requestWatermark).to.have.property('ticketId');
-    expect(result.data.requestWatermark.ticketId).to.be.a('string');
-    mlog.success('Got ticketId', result.data.requestWatermark.ticketId, 'for request with', query);
-  });
+      })
+      ticketId = response.data.requestWatermark.ticketId;
+      mlog.log('Got ticketId', ticketId);
+    })
 
+    it('Should give correct status', async function () {
+
+      // Wait a few seconds for the event to get propogated
+      await sleep(3000);
+
+      const query = `
+        {
+          document(ticketId:"${ticketId}") {
+            ticketId
+            status
+            updatedOn
+          }
+        }
+       `;
+
+      const response = await graphqlFetch(statusGraphqlEndpoint, {
+        query,
+        variables: null
+      });
+
+      expect(response).to.have.deep.property('data.document.ticketId');
+      expect(response).to.have.deep.property('data.document.status');
+      expect(response).to.have.deep.property('data.document.updatedOn');
+      const document = response.data.document;
+      expect(document.ticketId).to.equal(ticketId);
+      expect(document.status).to.be.oneOf(['FINISHED', 'PENDING', 'NONE']);
+      mlog.success('Got request status', document.status);
+
+    })
+
+  })
 });
 
-describe.skip("Status Service", function () {
-  it("Should be running", function () {
-    const response = chakram.get(statusService);
-    expect(response).to.have.status(200);
-    return chakram.wait();
+describe("Result Service", function () {
+  it("Should be running", async function () {
+    const response = await fetch(resultService);
+    expect(response.status).to.equal(200);
   });
 });
-
-describe.skip("Result Service", function () {
-  it("Should be running", function () {
-    const response = chakram.get(resultService);
-    expect(response).to.have.status(200);
-    return chakram.wait();
-  });
-});
-
