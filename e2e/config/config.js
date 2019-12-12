ab93ecaf02837202548f568d56330e1ef1159eb9
Jayadeep KM <kmjayadeep@gmail.com>
2019-12-12 08:27:19 +0530

    added e2e test case
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- e2e/config/config.js
+++ e2e/config/config.js
@@ -1,7 +1,7 @@
 
 const ticketingService = 'http://ticketing-service.default.34.82.141.113.nip.io'
 const statusService = 'http://status-service.default.34.82.141.113.nip.io';
-const resultService = 'http://status-service.default.34.82.141.113.nip.io';
+const resultService = 'http://result-service.default.34.82.141.113.nip.io';
 const ticketingGraphqlEndpoint = `${ticketingService}/graphql`;
 const statusGraphqlEndpoint = `${statusService}/graphql`
 const resultGraphqlEndpoint = `${resultService}/graphql`
--- e2e/test/index.spec.js
+++ e2e/test/index.spec.js
@@ -71,4 +71,63 @@ describe("Result Service", function () {
     const response = await resultService.ping();
     expect(response).to.equal('pong');
   });
+
+  describe('Test if result service is returning documents', () => {
+
+    let ticketId = '';
+
+    before(async () => {
+      mlog.pending('Creating a new request for checking result');
+      const response = await ticketingService.requestWatermark('book', 'my-test-title', 'me', 'business');
+      ticketId = response.data.requestWatermark.ticketId;
+      mlog.log('Got ticketId', ticketId);
+    });
+
+
+    it('should return document', async () => {
+      // wait for event to get propogated
+      await sleep(3000);
+      const response = await resultService.getResult(ticketId);
+
+      expect(response).to.have.deep.property('data.document');
+      const { document } = response.data;
+      expect(document).to.have.all.keys('ticketId', 'title', 'content', 'watermark', 'author');
+      expect(document.ticketId).to.equal(ticketId);
+      mlog.success('Got document result', `${document.title} ${document.content} ${document.watermark} ${document.author}`);
+    });
+
+  });
+});
+
+describe('e2e', function () {
+
+  it('Should watermark a given document', async () => {
+    const params = ['book', 'my-awesome-book', 'me', 'business'];
+    mlog.pending('Creating a new request with params', params);
+    const response = await ticketingService.requestWatermark(...params);
+    ticketId = response.data.requestWatermark.ticketId;
+    mlog.log('Got ticketId', ticketId);
+
+    let status = null;
+
+    do {
+      mlog.pending('Checking status for', ticketId);
+
+      const statusResponse = await statusService.getStatus(ticketId);
+      status = statusResponse.data.document ? statusResponse.data.document.status : null;
+
+      mlog.log('Current status is', status);
+      await sleep(1000);
+    } while (status != 'FINISHED');
+
+    const result = await resultService.getResult(ticketId);
+    const { watermark } = result.data.document;
+
+    expect(watermark).to.not.be.null;
+    expect(watermark).to.not.be.empty;
+
+    mlog.success('Got watermark', watermark);
+
+  })
+
 });
